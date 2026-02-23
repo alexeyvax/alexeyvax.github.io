5c6ded00e158ba4fd69257a238c1c239bc411e78
alexeyvax <alexeyvax1990@gmail.com>
2026-02-23 09:25:29 +0100

    Add seville-guide and tc pages

---
+++ seville-guide/174.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkseville_guide=self.webpackChunkseville_guide||[]).push([[174],{174:e=>{e.exports=JSON.parse('{"MainTitle":"Seville. Walking Guide","LogoAltText":"Logo","DescriptionTitle":"Description","ContactUsTitle":"Contacts","ContactUsHelperText":"To contact us, please send an email to","TermsAndConditionsTitle":"Terms & Conditions"}')}}]);
---
+++ seville-guide/terms-and-conditions/174.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkseville_guide_tc=self.webpackChunkseville_guide_tc||[]).push([[174],{174:e=>{e.exports=JSON.parse('{"MainTitle":"Seville. Walking Guide","LogoAltText":"Logo"}')}}]);
